f92063c76ecc6e159c0b116dd313228d38371c50
flow____ <gitkraken.swimsuit619@passmail.net>
2025-03-17 00:35:37 +0100

    Changed example script to current dir

--- example/Run-Video-Convert.sh
+++ example/Run-Video-Convert.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-python3  '/home/lukas/Documents/Video-to-web-video-format/video-converter.py' --root '/home/lukas/Nextcloud' --bitrate 8000000 --once false --keep false
+# https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+python3  '/home/flow/Documents/Video-to-web-video-format/video-converter.py' --root $SCRIPT_DIR --bitrate 8000000 --once true --keep false
